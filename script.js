3321b2b10952dcaf90250514c53c8b13a2cd874c
Tejdeep Nookala <iamtejdeepn@gmail.com>
2024-09-12 10:50:14 +0530

    amount and merchant name can now be updated, yet to add auto dateTime retrieval

---
+++ script.js
@@ -0,0 +1,42 @@
+const tick = document.querySelector("#blueTick");
+const dialogBox = document.querySelector("#newInput");
+const price = document.querySelector("#price");
+const newPrice = document.querySelector("#newPrice");
+const merchant = document.querySelector("#merchant");
+const newMerchant = document.querySelector("#newMerchant");
+const submit = document.querySelector("#submit")
+
+
+
+let clickCount = 0;
+
+tick.addEventListener('click',()=>{
+    clickCount++;
+    if(clickCount === 3){
+        clickCount = 0;
+
+        dialogBox.showModal();
+
+    }
+})
+
+const updateInfo = (function(){
+
+    function details(){
+        price.textContent = `₹${newPrice.value}`;
+        merchant.textContent = `Paid to ${newMerchant.value}`;
+    }
+
+    return {
+        details,
+    }
+
+})()
+
+submit.addEventListener('click',()=>{
+    event.preventDefault()
+
+    updateInfo.details();
+
+    dialogBox.close();
+})
